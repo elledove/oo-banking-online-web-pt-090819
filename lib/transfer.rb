e7c076ebb47191c27662454467da2d47060e6cce
Lauren Dove <elledoveinc@gmail.com>
2019-10-12 03:14:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,7 +37,11 @@ class Transfer
   end
   
   def reverse_transfer
-    
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
   
   end
   
