288ee94a8a23934d18c1fad712c8043addbb330f
Lauren Dove <elledoveinc@gmail.com>
2019-10-12 03:01:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
        #sub amount from sender
        #change the status to complete
        
-    if valid? && receiver.balance > sender.balance && @status == "pending"
+    if valid? && receiver.balance >= sender.balance && @status == "pending"
       sender.balance -= @transfer
        receiver.balance += @transfer
     @status = "complete"
